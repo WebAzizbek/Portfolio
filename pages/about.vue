9c739b97d5045b187c821363473f5ffd134c6129
WebAzizbek <azizbekweb3365@gmail.com>
2023-05-31 15:54:02 +0500

    portfolio

---
+++ pages/about.vue
@@ -0,0 +1,175 @@
+<template>
+    <div class="about-wrapper">
+        <div class="about">
+            <div class="me">
+                <div class="container">
+                    <div class="site-top-title">ABOUT</div>
+                    <div class="site-title">About Me</div>
+                    <div class="ocupation-wrapper">
+                        <h2 class="name">Azizbek Akramjonov</h2>
+                        <p class="ocupation">Web Developer</p>
+                    </div>
+                    <div class="me-about">
+                        <p>Hi, my name is Adriano Smith and I began using WordPress when it first began. I’ve spent most of
+                            my
+                            waking hours for the last ten years designing, programming and operating WordPress sites.
+                            <br>
+                            <br>
+                            One of my specialties is taking an idea from scratch and creating a full-fledged platform. I go
+                            beyond to produce sites with a unique, outstanding, contemporary look-and-feel. With extensive
+                            knowledge of web mechanics, I’m able to optimize complex integrations to require little-to-no
+                            maintenance while running on their own for years.
+                        </p>
+                    </div>
+                    <div class="born">
+                        <div class="wrapper">
+                            <div class="item">
+                                <span>Birthday :</span>   
+                                10.09.2006
+                            </div>
+                            <div class="item">
+                                <span>Age :</span>   
+                                17
+                            </div>
+                            <div class="item">
+                                <span>Address :</span>   
+                                S.Temur 338, Farg'ona, UZBEKISTAN
+                            </div>
+                            <div class="item">
+                                <span>Email :</span>   
+                                azizbekweb3365@gmail.com
+                            </div>
+                            <div class="item">
+                                <span>Phone :</span>   
+                                +998 91 131 01 05
+                            </div>
+                        </div>
+                        <div class="wrapper">
+                            <div class="item">
+                                <span>Nationality :</span>   
+                                UZBEKISTAN
+                            </div>
+                            <div class="item">
+                                <span>Study :</span>   
+                                3 school
+                            </div>
+                            <div class="item">
+                                <span>Degree :</span>   
+                                Junior
+                            </div>
+                            <div class="item">
+                                <span>Interest :</span>   
+                                Playing Basketball
+                            </div>
+                            <div class="item">
+                                <span>Freelance :</span>   
+                                Available
+                            </div>
+                        </div>
+                    </div>
+                    <button class="site-btn">Download CV</button>
+
+                </div>
+            </div>
+
+            <div class="skill">
+                <div class="container">
+                    <div class="wrapper">
+                        <h3 class="title">Programming Skills</h3>
+                        <ul>
+                            <li>
+                                <p class="text">JavaScript</p>
+                                <p class="num">70%</p>
+                            </li>
+                            <li>
+                                <p class="text">Vue 3</p>
+                                <p class="num">85%</p>
+                            </li>
+                            <li>
+                                <p class="text">Nuxt 3</p>
+                                <p class="num">75%</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="wrapper">
+                        <h3 class="title">Language Skills</h3>
+                        <ul>
+                            <li>
+                                <p class="text">English</p>
+                                <p class="num">80%</p>
+                            </li>
+                            <li>
+                                <p class="text">Russian</p>
+                                <p class="num">50%</p>
+                            </li>
+                            <li>
+                                <p class="text">French</p>
+                                <p class="num">25%</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                <div class="container">
+                    <div class="wrapper">
+                        <h4 class="title">Knowledge</h4>
+                        <ul>
+                            <li>
+                                <span></span>
+                                HTML, CSS
+                            </li>
+                            <li>
+                                <span></span>
+                                SCSS, SASS
+                            </li>
+                            <li>
+                                <span></span>
+                                JavaScript, TypeScript
+                            </li>
+                            <li>
+                                <span></span>
+                                JQuery, GSAP
+                            </li>
+                            <li>
+                                <span></span>
+                                Vue 3, Nuxt 3
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="wrapper">
+                        <h4 class="title">Interests</h4>
+                        <ul>
+                            <li>
+                                <span></span>
+                                Playing Basketball
+                            </li>
+                            <li>
+                                <span></span>
+                                Learning English
+                            </li>
+                            <li>
+                                <span></span>
+                                Create Design
+                            </li>
+                            <li>
+                                <span></span>
+                                Custom Website
+                            </li>
+                            <li>
+                                <span></span>
+                                Learn Ecommerce
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
