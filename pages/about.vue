d8f54d77b636e72f4963be2a97b78b59a0819d87
DevAzizbek <azizbekakramjonov0910@gmail.com>
2023-08-09 09:11:25 +0500

    Portfolio

---
+++ pages/about.vue
@@ -0,0 +1,178 @@
+<template>
+    <div class="about-wrapper">
+        <div class="about">
+            <div class="me">
+                <div class="container">
+                    <div class="site-top-title">ABOUT</div>
+                    <div class="site-title">About Me</div>
+                    <div class="ocupation-wrapper">
+                        <h2 class="name">Azizbek Akramjonov</h2>
+                        <p class="ocupation">Web Developer</p>
+                    </div>
+                    <div class="me-about">
+                        <p>Hi, my name is Akramjonov Azizbek and I began using Vue when it first began. I’ve spent most of
+                            my waking hours for the last one years Frontend.
+
+                            <br>
+                            <br>
+
+                            One of my specialties is taking an idea from scratch and building a complete platform.
+                            I produce unique, stunning, modern looking sites.
+                            With extensive knowledge of frontend, I have been able to build sites independently for years.
+                            I love bringing ideas to life through created visuals and meaningful animations, playing with colors, composition, typography and details.
+                            I will gain experience in this field from 2022
+
+                        </p>
+                    </div>
+                    <div class="born">
+                        <div class="wrapper">
+                            <div class="item">
+                                <span>Birthday :</span>   
+                                10.09.2006
+                            </div>
+                            <div class="item">
+                                <span>Age :</span>   
+                                17
+                            </div>
+                            <div class="item">
+                                <span>Address :</span>   
+                                S.Temur 338, Farg'ona, UZBEKISTAN
+                            </div>
+                            <div class="item">
+                                <span>Email :</span>   
+                                azizbekweb3365@gmail.com
+                            </div>
+                            <div class="item">
+                                <span>Phone :</span>   
+                                +998 91 131 01 05
+                            </div>
+                        </div>
+                        <div class="wrapper">
+                            <div class="item">
+                                <span>Nationality :</span>   
+                                UZBEKISTAN
+                            </div>
+                            <div class="item">
+                                <span>Study :</span>   
+                                3 school
+                            </div>
+                            <div class="item">
+                                <span>Degree :</span>   
+                                Junior
+                            </div>
+                            <div class="item">
+                                <span>Interest :</span>   
+                                Playing Basketball
+                            </div>
+                            <div class="item">
+                                <span>Freelance :</span>   
+                                Available
+                            </div>
+                        </div>
+                    </div>
+                    <button class="site-btn">Download CV</button>
+
+                </div>
+            </div>
+
+            <div class="skill">
+                <div class="container">
+                    <div class="wrapper">
+                        <h3 class="title">Programming Skills</h3>
+                        <ul>
+                            <li>
+                                <p class="text">JavaScript</p>
+                                <p class="num">70%</p>
+                            </li>
+                            <li>
+                                <p class="text">Vue 3</p>
+                                <p class="num">85%</p>
+                            </li>
+                            <li>
+                                <p class="text">Nuxt 3</p>
+                                <p class="num">75%</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="wrapper">
+                        <h3 class="title">Language Skills</h3>
+                        <ul>
+                            <li>
+                                <p class="text">English</p>
+                                <p class="num">80%</p>
+                            </li>
+                            <li>
+                                <p class="text">Russian</p>
+                                <p class="num">50%</p>
+                            </li>
+                            <li>
+                                <p class="text">French</p>
+                                <p class="num">25%</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                <div class="container">
+                    <div class="wrapper">
+                        <h4 class="title">Knowledge</h4>
+                        <ul>
+                            <li>
+                                <span></span>
+                                HTML, CSS
+                            </li>
+                            <li>
+                                <span></span>
+                                SCSS, SASS
+                            </li>
+                            <li>
+                                <span></span>
+                                JavaScript, TypeScript
+                            </li>
+                            <li>
+                                <span></span>
+                                JQuery, GSAP
+                            </li>
+                            <li>
+                                <span></span>
+                                Vue 3, Nuxt 3
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="wrapper">
+                        <h4 class="title">Interests</h4>
+                        <ul>
+                            <li>
+                                <span></span>
+                                Playing Basketball
+                            </li>
+                            <li>
+                                <span></span>
+                                Learning English
+                            </li>
+                            <li>
+                                <span></span>
+                                Create Design
+                            </li>
+                            <li>
+                                <span></span>
+                                Custom Website
+                            </li>
+                            <li>
+                                <span></span>
+                                Learn Ecommerce
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
---
+++ pages/portfolio.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="portfolio">
+        <div class="container">
+            <div class="site-top-title">Portfolio</div>
+            <div class="site-title">My Portfolio</div>
+            <div class="wrapper">
+                <div class="item">
+                    <img src="~/assets/img/png/ux-ui.png" alt="">
+                    <a target="_blank" href="https://ux-ui-akramjonov.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/png/fargonaliklar.png" alt="">
+                    <a target="_blank" href="https://fargonaliklar.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/infi.jpg" alt="">
+                    <a target="_blank" href="https://infinizai-web.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/moxa.jpg" alt="">
+                    <a href="https://moxa-web.netlify.app/" target="_blank" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/alkk.jpg" alt="">
+                    <a href="https://alkk-akramjonov.netlify.app/" target="_blank" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/pix.jpg" alt="">
+                    <a target="_blank" href="https://pixerveb.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/int.jpg" alt="">
+                    <a target="_blank" href="https://an-interno-web.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/ver.jpg" alt="">
+                    <a target="_blank" href="https://vercuc.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/tem.jpg" alt="">
+                    <a target="_blank" href="https://templates-web.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/cofe.jpg" alt="">
+                    <a target="_blank" href="https://cofe-roasters.netlify.app/index.html" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/aka.jpg" alt="">
+                    <a target="_blank" href="https://the-akademnashr-web.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/ans.jpg" alt="">
+                    <a target="_blank" href="https://ansor-web.netlify.app/index.html" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/sai.jpg" alt="">
+                    <a target="_blank" href="https://sainte.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/exp.jpg" alt="">
+                    <a target="_blank" href="https://exploler-web.netlify.app/" class="more">View more</a>
+                </div>
+                <div class="item">
+                    <img src="~/assets/img/jpg/pro.jpg" alt="">
+                    <a target="_blank" href="https://the-product-web.netlify.app/" class="more">View more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
